89d1db59bb8f8453cd8410d651849d92c4502684
yuhixyz <yuhixyz@outlook.com>
2024-04-16 14:14:57 +0800

    fix: NODE_ENV=dev指定开发环境

--- src/services/code.js
+++ src/services/code.js
@@ -1,10 +1,12 @@
 import axios from "axios"
 
-const baseUrl = 'http://localhost:3001/api/code/run'
+const baseUrl = process.env.NODE_ENV == 'dev' ?
+    'http://localhost:3001/api/code/run'
+    : '/api/code/run';
 
 const runCode = async (code, inputText) => {
     const requestParam = {
-        "code" : code,
+        "code": code,
         "inputText": inputText
     }
     const response = await axios.post(baseUrl, requestParam)
