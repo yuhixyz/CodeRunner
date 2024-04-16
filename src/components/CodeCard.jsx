d118c783951c6d4c5ace31c146e20668984a8249
yuhixyz <yuhixyz@outlook.com>
2024-04-16 20:43:02 +0800

    feature: 实现编辑模式设置缓存浏览器

--- src/components/CodeCard.jsx
+++ src/components/CodeCard.jsx
@@ -3,24 +3,27 @@ import AceEditor from 'react-ace'
 import { Button } from 'antd/es/radio';
 import 'ace-builds/src-noconflict/mode-c_cpp'
 import 'ace-builds/src-noconflict/keybinding-vim'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
 const CodeCard = ({ code, handleCodeChange }) => {
     const [keyboardHandler, setKeyboardHandler] = useState('vim')
-    const [mode, setMode] = useState('Vim');
+
+    useEffect(() => {
+       setKeyboardHandler(localStorage.getItem('keyboardHandler'))
+    }, [])
 
     const inputModeSwitch = () => {
         if (keyboardHandler == 'vim') {
-            setKeyboardHandler('')
-            setMode('Standard')
+            setKeyboardHandler('standard')
+            localStorage.setItem('keyboardHandler', 'standard')
         } else {
             setKeyboardHandler('vim')
-            setMode('Vim')
+            localStorage.setItem('keyboardHandler', 'vim')
         }
     }
     return (
-        <Card title="Code Runner" extra={<Button onClick={inputModeSwitch}>{mode}</Button>}>
+        <Card title="Code Runner" extra={<Button onClick={inputModeSwitch}>{keyboardHandler}</Button>}>
             <AceEditor
                 width='92vw'
                 height='60vh'
