b6ab22a2b3ef74c85b149e9b5665bdbbf2c376a2
yuhixyz <yuhixyz@outlook.com>
2024-04-16 16:32:02 +0800

    fix: 修复运行错误信息输出

--- routes/code.js
+++ routes/code.js
@@ -5,14 +5,14 @@ const { exec } = require('child_process');
 codeRouter.post('/run', (req, res) => {
     const code = req.body.code;
     const inputText = req.body.inputText;
-    exec(`echo '${code}' > temp.cpp && g++ temp.cpp -o temp.exe`, (error, stdout, stderr) => {
+    exec(`echo '${code}' > test.cpp && g++ test.cpp -o test`, (error, stdout, stderr) => {
         if (error) {
             res.send({ "success": false, "type": "Compile Error", "data": stderr }); // 编译错误时返回错误信息
         } else {
             // 运行编译后的可执行文件
-            exec(`echo '${inputText}' > input.txt && ./temp.exe < input.txt`, (runError, runStdout, runStderr) => {
+            exec(`echo '${inputText}' > input.txt && ./test < input.txt`, (runError, runStdout, runStderr) => {
                 if (runError) {
-                    res.send({ "success": false, "type": "Runtime Error", "error": runStderr }); // 运行错误时返回错误信息
+                    res.send({ "success": false, "type": runStderr, "data": stderr }); // 运行错误时返回错误信息
                 } else {
                     res.send({ "success": true, "type": "Finished", "data": runStdout });
                 }
