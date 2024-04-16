2d819e53b530a7383b7c44c69fdda47438099235
yuhixyz <yuhixyz@outlook.com>
2024-04-16 16:29:25 +0800

    feature: 添加前端静态文件

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const codeRouter = require('./routes/code');
 const cors = require('cors');
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json());
 app.use('/api/code', codeRouter);
