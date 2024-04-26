ebd7c0a012a487e87c080244c80b37e9b10d80ca
yuhixyz <yuhixyz@outlook.com>
2024-04-26 18:05:07 +0800

    style: 添加Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,19 @@
-import CodeCard from './components/CodeCard'
+import CodeCard from './components/CodeCard';
+import { Layout } from 'antd';
+
+const { Footer } = Layout;
 
 const App = () => {
     return (
         <div>
-            {/* Navbar */}
-            <CodeCard />
-            {/* Footer */}
+            <Layout>
+                <CodeCard />
+                <Footer style={{ textAlign: 'center' }}>
+                    <a href='https://github.com/yuhixyz/CodeRunner'>Code Runner</a> ©{new Date().getFullYear()} Created by <a href='https://github.com/yuhixyz'>yuhixyz</a>
+                </Footer>
+            </Layout>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
