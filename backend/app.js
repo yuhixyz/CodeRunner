10c966431c9ea06f9c972f97e827c792ce102e58
yuhixyz <yuhixyz@outlook.com>
2024-05-07 15:04:00 +0800

    feature: jh要的看板娘v1

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const userRouter = require('./routes/user');
 const config = require('./utils/config');
 const mongoose = require('mongoose')
+const path = require('path');
 
 console.log('connecting to', config.MONGODB_URI)
 
@@ -22,5 +23,10 @@ app.use(cors())
 app.use(express.json());
 app.use('/api/users', userRouter);
 app.use('/api/code', codeRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 
 module.exports = app;
--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,12 +1,30 @@
 import { Result } from 'antd';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
-const NotFound = () => (
-    <Result
-        status="404"
-        title="404"
-        subTitle="页面不存在"
-        extra={<Link to='/' >回到主页</Link>}
-    />
-);
+const NotFound = () => {
+
+    useEffect(() => {
+        // 创建script标签
+        const script = document.createElement('script');
+        script.src = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/autoload.js";
+        script.async = true;
+        script.defer = true;
+        script.crossOrigin = 'anonymous';
+        document.body.appendChild(script);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <Result
+            status="404"
+            title="404"
+            subTitle="页面不存在"
+            extra={<Link to='/' >回到主页</Link>}
+        />
+    );
+};
+
 export default NotFound;
